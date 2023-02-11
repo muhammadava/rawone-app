c4a0362ab1aded584c5fcc5dd4a5f5f5ca34b9f3
muhammadava <muhammadava861@gnail.com>
2023-02-11 09:32:12 +0700

    Membuat tampilan seluruh laporan

---
+++ resources/views/dashboard/allReports/reports.blade.php
@@ -0,0 +1,11 @@
+@extends('dashboard.master')
+
+@section( 'content' )
+    <div class="container">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div><!-- container -->
+@endsection
